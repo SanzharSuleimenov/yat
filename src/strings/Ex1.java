4aeffdfa66c79b3d945c8f166086a4cb2f8bb279
Sanzhar Suleimenov <s.suleimenov@parqour.com>
2024-01-30 20:55:50 +0600

    Additional String tasks observation

--- src/strings/Ex1.java
+++ src/strings/Ex1.java
@@ -1,2 +1,17 @@
-package strings;public class Ex1 {
+package strings;
+
+import java.util.Scanner;
+
+public class Ex1 {
+  private static Scanner scanner = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    String ip = scanner.nextLine();
+    String defanged = defangIPaddr(ip);
+    System.out.println(defanged);
+  }
+  public static String defangIPaddr(String address) {
+    // it's your code
+    return address.replace(".","[.]");
+  }
 }
